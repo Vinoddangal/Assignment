f92526576807064067ad595734fa25f195286651
Vinoddangal <dangalbinod555@gmail.com>
2019-12-25 18:43:54 +0545

    Assignment and Table Update

--- assignment/Object_Contain_Info/object.js
+++ assignment/Object_Contain_Info/object.js
@@ -2,13 +2,16 @@ var obj = {
     name: 'Vinod',    
     address: 'Thali',
     email: 'vinod@gmail.com',
-    interes: 'cricket',
-    education: 'BIT'
+    interest: 'cricket',
+    education : {name: ["Aakash Deep","SANN","KIST"], enrolled: [2070,2073,2074] }
 };
-var keys = Object.keys(obj);
-for (var i = 0; i < keys.length; i++)
-{    
-    var key = keys[i];    
-    var value = obj[key];
-    console.log(key, value);
+console.log(obj.name);
+console.log(obj.address);
+console.log(obj.email);
+console.log(obj.interest);
+
+for(i=0; i<obj.education.name.length; i++)
+{
+    console.log(obj.education.name[i],":");
+    console.log(obj.education.enrolled[i]);
 }
