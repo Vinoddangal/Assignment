439529ca43659dfb173218a43ffc66d7ae7d3a4a
Vinoddangal <dangalbinod555@gmail.com>
2019-12-28 13:08:06 +0545

    Assignment Update

--- assignment/Box_Moving_Render/move.js
+++ assignment/Box_Moving_Render/move.js
@@ -8,6 +8,8 @@ document.body.appendChild(box);
 
 var MoveBy = 10;
 
+
+//load the box
 window.addEventListener('load', () => {
     box.style.position = 'absolute';
     box.style.top = 0;
@@ -15,20 +17,20 @@ window.addEventListener('load', () => {
 });
 
 
-
+//Moves box using arrow key
 window.addEventListener('keyup', (event) => {
     switch (event.key) {
         case 'ArrowLeft' :
-            box.style.left = parseInt(box.style.left) - MoveBy + 'px';
+            box.style.left = parseInt(box.style.left) - MoveBy + 'px'; //Move left
             break;
         case 'ArrowRight' :
-            box.style.left = parseInt(box.style.left) + MoveBy + 'px';
+            box.style.left = parseInt(box.style.left) + MoveBy + 'px'; //move Right
         break;
         case 'ArrowUp' : 
-            box.style.top = parseInt(box.style.top) - MoveBy + 'px';
+            box.style.top = parseInt(box.style.top) - MoveBy + 'px'; // Move Up
             break;
         case 'ArrowDown' :
-            box.style.top = parseInt(box.style.top) + MoveBy + 'px';
+            box.style.top = parseInt(box.style.top) + MoveBy + 'px'; // Move Down
             break;
     }
 });
